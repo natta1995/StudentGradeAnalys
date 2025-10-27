c429183a1f2afa64e0398819aaa69c76d18b57c1
natta1995 <natalie.hallerdal@yahoo.se>
2025-10-27 12:15:18 +0100

    Student class done

---
+++ Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGradeAnalys
+{
+    public class Student
+    {
+        public string Name;
+        public string Grade;
+        public bool Passed; 
+
+        public Student(string name, string grade, bool passed)
+        {
+            Name = name;
+            Grade = grade;
+            Passed = passed; 
+        }
+
+        public void StudentInformation()
+        {
+            Console.WriteLine($"Student name: { Name }.\nStudent grade: {Grade }.\nStudent passed: {Passed}");
+        }
+    }
+
+
+}
---
+++ file1.md
@@ -0,0 +1,7 @@
+﻿🎓 1. Studentbetyg-Analys
+Skapa en klass Student med egenskaperna Name, Grade och Passed (bool).
+ ✅ Gör en lista av studenter och använd:
+.FindAll() för att hitta de som klarade kursen
+.Average() för att räkna ut medelbetyg
+.Sort() för att sortera efter betyg
+ 💡 Extra: Visa de 3 bästa studenterna med .Take(3).
