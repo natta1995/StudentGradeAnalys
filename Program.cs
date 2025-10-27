d1a0b71bfd362f3e1bda69dad2e8aabd304edf46
natta1995 <natalie.hallerdal@yahoo.se>
2025-10-27 14:37:55 +0100

    Added - Show all students that passed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿namespace StudentGradeAnalys
+
+//    🎓 1. Studentbetyg-Analys Skapa en klass Student med egenskaperna Name, Grade och Passed(bool).
+//✅ Gör en lista av studenter och använd: .FindAll() för att hitta de som klarade kursen.Average() 
+//    för att räkna ut medelbetyg.Sort() för att sortera efter betyg 💡 Extra: Visa de 3 bästa studenterna med.Take(3)
 {
     internal class Program
     {
@@ -20,6 +24,16 @@
             Console.WriteLine("All students:");
             students.ForEach(s => s.StudentInformation());
             Console.WriteLine();
+
+            var studentsThatPassed = students.FindAll(s => s.Passed == true);
+            Console.WriteLine("Students that passed this class: ");
+            studentsThatPassed.ForEach(s => {
+            Console.WriteLine($"{s.Name} passed whit the grade {s.Grade}." );
+            });
+            
+
+            
         }
+
     }
 }
--- Student.cs
+++ Student.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace StudentGradeAnalys
+
+
 {
     public class Student
     {
